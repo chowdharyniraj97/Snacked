3a8fe19911db436c730256d5228fe20c5f3b956f
NIRAJ CHOWDHARY <chowdharyniraj97@gmail.com>
2020-03-22 02:32:06 -0400

    added json text?

--- app.js
+++ app.js
@@ -19,7 +19,14 @@ class Products {
     try {
       let result = await fetch("products.json");
       let data = await result.json();
-      return data;
+      let products = data.items;
+      products = products.map(item => {
+        const { title, price } = item.fields;
+        const { id } = item.sys;
+        const image = item.fields.image.fields.file.url;
+        return { title, price, id, image };
+      });
+      return products;
     } catch (error) {
       console.log(error);
     }
@@ -31,6 +38,9 @@ class UI {}
 class Storage {}
 
 document.addEventListener("DOMContentLoaded", () => {
-  const ui = new UIEvent();
+  const ui = new UI();
   const products = new Products();
+
+  //get all products
+  products.getProducts().then(data => console.log(data));
 });
