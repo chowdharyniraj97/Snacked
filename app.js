11574bf088564b073d8e851f9a5a9ea6c41aad08
NIRAJ CHOWDHARY <chowdharyniraj97@gmail.com>
2020-03-22 02:21:21 -0400

    js functions added

--- app.js
+++ app.js
@@ -2,3 +2,35 @@
 
 const cardBtn = document.querySelector(".card-btn");
 const closecartbtn = document.querySelector(".close-cart");
+const clearCartBtn = document.querySelector(".clear-cart");
+const cartDom = document.querySelector(".cart");
+const cartOverlay = document.querySelector(".cart-overlay");
+const cartItems = document.querySelector(".cart-items");
+const cartTotal = document.querySelector(".cart-total");
+const cartContent = document.querySelector(".cart-content");
+const productsDOM = document.querySelector(".products-center");
+
+//cart
+
+let cart = [];
+
+class Products {
+  async getProducts() {
+    try {
+      let result = await fetch("products.json");
+      let data = await result.json();
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
+
+class UI {}
+
+class Storage {}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const ui = new UIEvent();
+  const products = new Products();
+});
