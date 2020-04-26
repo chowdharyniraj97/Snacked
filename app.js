a231934d9723a9c478acb23559f374ca492c3080
NIRAJ CHOWDHARY <chowdharyniraj97@gmail.com>
2020-04-25 23:52:21 -0400

    added side menu

--- app.js
+++ app.js
@@ -260,6 +260,15 @@ class Storage {
       : [];
   }
 }
+function openSlideMenu() {
+  document.getElementById("side-menu").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeSlideMenu() {
+  document.getElementById("side-menu").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
 
 document.addEventListener("DOMContentLoaded", () => {
   const ui = new UI();
